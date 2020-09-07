1aaf4517faa2de9ef4a68649791ad79727d6cbd7
Parag Adhau <adhauparag64@gmail.com>
2020-09-07 17:26:21 +0530

    Added :Test case for wrong file type

--- src/test/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyserTest.java
@@ -8,29 +8,40 @@ import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
 
-        private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-        private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-
-        @Test
-        public void givenIndianCensusCSVFileReturnsCorrectRecords() {
-            try {
-                CensusAnalyser censusAnalyser = new CensusAnalyser();
-                int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-                Assert.assertEquals(29,numOfRecords);
-            } catch (CensusAnalyserException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Test
-        public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
-            try {
-                CensusAnalyser censusAnalyser = new CensusAnalyser();
-                ExpectedException exceptionRule = ExpectedException.none();
-                exceptionRule.expect(CensusAnalyserException.class);
-                censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
-            } catch (CensusAnalyserException e) {
-                Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
-            }
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_FILE_TYPE = "./src/test/resources/IndiaStateCensusData.xlsx";
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
         }
     }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    @Test
+    public void givenIndiaCensusData_WithWrongFileType_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_FILE_TYPE,e.type);
+        }
+    }
+}
