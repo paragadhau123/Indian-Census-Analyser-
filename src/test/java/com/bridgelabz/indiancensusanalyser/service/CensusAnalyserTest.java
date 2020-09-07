6148ce8a82d5cd13cd3cec351f20efae9fa1a894
Parag Adhau <adhauparag64@gmail.com>
2020-09-07 22:16:53 +0530

    Added :Test case for wrong csv file type

--- src/test/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyserTest.java
@@ -92,7 +92,19 @@ public class CensusAnalyserTest {
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             System.out.println(e.getMessage());
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.STATE_CODE_FILE_PROBLEM, e.type);
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    @Test
+    public void givenIndiaStateCodeFile_WithWrongFileType_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
 }
