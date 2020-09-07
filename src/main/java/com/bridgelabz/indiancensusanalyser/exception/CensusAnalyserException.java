a7712486c042a9ff31fcccfce30d7102471ba0dd
Parag Adhau <adhauparag64@gmail.com>
2020-09-07 16:45:36 +0530

    Added :Two test cases CSV operation

--- src/main/java/com/bridgelabz/indiancensusanalyser/exception/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/indiancensusanalyser/exception/CensusAnalyserException.java
@@ -5,15 +5,11 @@ public class CensusAnalyserException extends Exception{
         CENSUS_FILE_PROBLEM
     }
 
-    ExceptionType type;
+    public ExceptionType type;
 
     public CensusAnalyserException(String message, ExceptionType type) {
         super(message);
         this.type = type;
     }
 
-    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
-        super(message, cause);
-        this.type = type;
-    }
 }
--- src/main/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyser.java
@@ -12,25 +12,23 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CensusAnalyser {
-    public static void main (String [] args) throws CensusAnalyserException {
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try {
-            Reader reader = Files.newBufferedReader(Paths.get("D:\\parag\\Fellowship\\Indian-Census-Analyser-\\IndiaStateCensusData.csv"));
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
             int namOfEateries = 0;
             while (censusCSVIterator.hasNext()) {
                 namOfEateries++;
                 IndiaCensusCSV censusData = censusCSVIterator.next();
-                System.out.println(namOfEateries);
             }
-            //return namOfEateries;
+            return namOfEateries;
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
 }
-
