e690a5d2aa2dc9e9867edac4ed09dc97e6535df4
Parag Adhau <adhauparag64@gmail.com>
2020-09-07 21:32:10 +0530

    Added :Ability for analyser to load indian state code information from csv file

--- src/main/java/com/bridgelabz/indiancensusanalyser/exception/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/indiancensusanalyser/exception/CensusAnalyserException.java
@@ -5,6 +5,8 @@ public class CensusAnalyserException extends Exception {
 
     public enum ExceptionType {
         CENSUS_FILE_PROBLEM, WRONG_FILE_TYPE, RUN_TIME_EXCEPTION
+        CENSUS_FILE_PROBLEM, WRONG_FILE_TYPE, RUN_TIME_EXCEPTION,STATE_CODE_FILE_PROBLEM
+
     }
 
 
--- src/test/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indiancensusanalyser/service/CensusAnalyserTest.java
@@ -11,6 +11,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_FILE_TYPE = "./src/test/resources/IndiaStateCensusData.xlsx";
+    /*Test Cases For Reading indiaStateCensusData.csv*/
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -69,4 +70,6 @@ public class CensusAnalyserTest {
             Assert.assertEquals("Error capturing CSV header!",e.getMessage());
         }
     }
+    /*Test Cases For Reading IndiaStateCode.csv*/
+
 }
