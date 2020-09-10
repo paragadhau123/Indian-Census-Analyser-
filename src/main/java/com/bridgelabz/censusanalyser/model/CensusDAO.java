8c34ecbfcf397131d85f20dfe1eeec723466c5cf
Parag Adhau <adhauparag64@gmail.com>
2020-09-10 15:57:06 +0530

    Added :DAO for US ceensus data

--- src/main/java/com/bridgelabz/censusanalyser/model/CensusDAO.java
+++ src/main/java/com/bridgelabz/censusanalyser/model/CensusDAO.java
@@ -1,14 +1,28 @@
 package com.bridgelabz.censusanalyser.model;
 
 public class CensusDAO {
+    // StateCodeCSV Fields
     public int srNo;
     public int tin;
     public String stateCode;
     public String stateName;
+
+    // IndianCensusCSV Fileds
     public int densityPerSqKm;
     public int population;
     public int area;
     public String state;
+
+    // USCensusCSV Fields
+    public int housingUnits;
+    public float populationDensity;
+    public float waterArea;
+    public float landArea;
+    public float totalArea;
+    public float housingDensity;
+    public int usPopulation;
+    public String usState;
+    public String stateID;
 
     public CensusDAO(IndiaCensusCSV indiaCensusCSV) {
         state = indiaCensusCSV.state;
@@ -22,5 +36,18 @@ public class CensusDAO {
         stateCode = indiaStateCodeCSV.stateCode;
         tin = indiaStateCodeCSV.tin;
         srNo = indiaStateCodeCSV.srNo;
+    }
+
+    public CensusDAO(UScensusCSV usCensusCSV) {
+        stateID = usCensusCSV.stateID;
+        usState = usCensusCSV.state;
+        usPopulation = usCensusCSV.population;
+        housingDensity = usCensusCSV.housingDensity;
+        totalArea = usCensusCSV.totalArea;
+        landArea = usCensusCSV.landArea;
+        waterArea = usCensusCSV.waterArea;
+        populationDensity = usCensusCSV.populationDensity;
+        housingUnits = usCensusCSV.housingUnits;
+
     }
 }
